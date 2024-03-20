f73d081f35ffa323fb3665af80fb76175cd54ebc
YoWuzzup <YoWuzzup@live.com>
2024-03-21 00:13:05 +0100

    added redux slice for new order

---
+++ frontend/src/redux/slices/newOrderSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type SliceState = null | {
+  items: string[];
+  additionalData: {
+    message: string;
+    selfPickup: boolean;
+    totalPrice: number;
+  };
+  contacts: {};
+  date: string;
+};
+
+const newOrderSlice = createSlice({
+  name: "newOrder",
+  initialState: null as SliceState,
+  reducers: {
+    addToNewOrder(state, action) {
+      return { ...action.payload };
+    },
+  },
+});
+
+export const { addToNewOrder } = newOrderSlice.actions;
+export default newOrderSlice.reducer;
